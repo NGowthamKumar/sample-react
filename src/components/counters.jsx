46158c895357cb69580f130ad28bedd03f2c55bf
GowthamKumar_21 <gowthamkumar.nandakumar@bonbloc.com>
2021-06-21 14:17:15 +0530

    Initial commit

---
+++ src/components/counters.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+    render() { 
+        return (<React.Fragment>
+            <button 
+            onClick = { this.props.onReset} 
+            className ='btn btn-primary btn-sm m-2' > Reset </button>
+                    {this.props.counters.map(counter => <Counter key = {counter.id}
+                                                                 counter = {counter}
+                                                                 onDelete = {this.props.onDelete}
+                                                                 onIncrement = {this.props.onIncrement}
+                                                                 onDecrement = {this.props.onDecrement}
+                                                        />
+                                            )
+                    }
+                </React.Fragment>
+        );
+    };
+}
+ 
+export default Counters;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const NavBar = (props) => {
+    return (  
+        <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+        NavBar <span className="badge badge-pill badge-secondary"> Total Items : {props.totalCounters}</span>
+        </a>
+        </div>
+        </nav>
+    );
+}
+
+ 
+export default NavBar;
